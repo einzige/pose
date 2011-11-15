99d54f65c4d86673ca55b4722e6a7bc86af5e0e2
Kevin Goslar <kevin.goslar@gmail.com>
2011-11-14 18:43:21 -0800

    Generator for migrations.

---
+++ lib/generators/pose_generator.rb
@@ -0,0 +1,16 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+
+class PoseGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+  source_root File.expand_path('../templates', __FILE__)
+  
+  def create_migration_file
+    migration_template 'migration.rb', 'db/migrate/pose.rb'
+  end
+  
+  def self.next_migration_number(path)
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
+  end
+
+end
