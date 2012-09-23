b2442d519a63232fa8fb7edd91264654101085d9
Kevin Goslar <kevin.goslar@originate.com>
2012-09-23 08:36:46 -0700

    fixes bugs

--- lib/pose/models/pose_word.rb
+++ lib/pose/models/pose_word.rb
@@ -1,7 +1,7 @@
 # A single word in the search index.
 class PoseWord < ActiveRecord::Base
   has_many :pose_assignments
-  attr_accessible :word
+  attr_accessible :text
 
   def self.remove_unused_words progress_bar = nil
     PoseWord.find_each(include: [:pose_assignments], batch_size: 5000) do |pose_word|
