88e34e2175b71dfaba6285fbd1bdd6f635011a5a
Sergei Zinin <zinin@xakep.ru>
2013-04-15 01:07:06 +0800

    Refactor query: remove duplicates.

--- lib/pose/query.rb
+++ lib/pose/query.rb
@@ -1,4 +1,5 @@
 module Pose
+  # TODO: refactor
   class Query
     attr_reader :classes, :query_string, :options
 
@@ -16,52 +17,30 @@ module Pose
     end
 
     # @param [Class] klass
-    # @return [ActiveRecord::Relation]
-    def relation_for(klass)
-      unless classes.include?(klass)
-        raise ArgumentError, "The class #{klass.name} is not included in this query"
-      end
-
-      ids = result_classes_and_ids[klass.name]
-
-      result = klass.where(id: ids)
-      result = result.limit(options[:limit])
-
-      if options[:where].present?
-        options[:where].each do |scope|
-          result = result.where(scope)
-        end
-      end
-
-      result
-    end
-
+    # @return [Array<Integer>]
     def ids_for(klass)
       ids = result_classes_and_ids[klass.name]
 
       if options[:where]
-        query = klass.scoped
-        query = query.limit(options[:limit])
-
-        options[:where].each do |scope|
-          query = query.select('id').where('id IN (?)', ids).where(scope)
-        end
-
-        klass_ids = klass.connection.select_values(query.to_sql)
-        klass_ids.map(&:to_i)
+        query = relation_for(klass)
+        klass.connection.select_values(query.to_sql).map(&:to_i)
       else
-        if options[:limit]
-          ids.first(options[:limit])
-        else
-          ids
-        end
+        options[:limit] ? ids.first(options[:limit]) : ids
       end
     end
 
+    # @param [Class] klass
+    # @return [ActiveRecord::Relation]
+    def relation_for(klass)
+      ids = result_classes_and_ids[klass.name]
+      apply_where_on_scope(klass.where(id: ids)).limit(options[:limit])
+    end
+
     # Gets the ids of the results.
+    # TODO: remove Helpers
     # @return [Hash<String, Array<Integer>>]
     def result_classes_and_ids
-      {}.tap do |classes_and_ids|
+      @result_classes_and_ids ||= {}.tap do |classes_and_ids|
         Helpers.query_terms(query_string).each do |query_word|
           Helpers.search_classes_and_ids_for_word(query_word, class_names).each do |class_name, ids|
             Helpers.merge_search_result_word_matches classes_and_ids, class_name, ids
@@ -70,10 +49,7 @@ module Pose
       end
     end
 
-    def results
-      @results ||= search
-    end
-
+    # @return [Hash<Class, Array<Integer>>]
     def result_ids
       {}.tap do |result|
         result_classes_and_ids.each do |class_name, ids|
@@ -83,7 +59,14 @@ module Pose
       end
     end
 
-    # @return [ActiveRecord::Relation]
+    # Cached #search
+    # @see #search
+    # @return [Hash<Class, ActiveRecord::Relation>]
+    def results
+      @results ||= search
+    end
+
+    # @return [Hash<Class, ActiveRecord::Relation>]
     def search
       @results = {}.tap do |result|
         result_classes_and_ids.each do |class_name, ids|
@@ -91,6 +74,19 @@ module Pose
           result[result_class] = ids.empty? ? [] : relation_for(result_class)
         end
       end
+    end
+
+    private
+
+    # @param [ActiveRecord::Relation]
+    # @return [ActiveRecord::Relation]
+    # TODO: remove?
+    def apply_where_on_scope(scope)
+      result = scope.clone
+      if options[:where].present?
+        options[:where].each { |scope| result = result.where(scope) }
+      end
+      result
     end
   end
 end
