025bb6eaf739b4ae1a16b3957439b0f5edf5019d
Oleg Pudeyev <oleg@bsdpower.com>
2013-05-08 18:25:14 -0400

    Update pose:install generator to install the current schema

--- lib/generators/pose/install/templates/install_migration.rb
+++ lib/generators/pose/install/templates/install_migration.rb
@@ -2,12 +2,12 @@ class InstallPose < ActiveRecord::Migration
 
   def self.up
     create_table "pose_assignments" do |t|
-      t.integer "pose_word_id",            null: false
+      t.integer "word_id",            null: false
       t.integer "posable_id",              null: false
       t.string  "posable_type", limit: 40, null: false
     end
 
-    add_index "pose_assignments", :pose_word_id
+    add_index "pose_assignments", :word_id
     add_index "pose_assignments", :posable_id
 
     create_table "pose_words" do |t|
