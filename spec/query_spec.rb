e0e91838f670f9dbb5b8054ded1cb051d84965ad
Kevin Goslar <kevin.goslar@originate.com>
2013-06-01 11:42:17 -0700

    Tests for Pose::Query

---
+++ spec/query_spec.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+require "spec_helper"
+
+describe Pose::Query do
+
+  let(:subject) { Pose::Query.new [PosableOne, [PosableTwo]], 'query string' }
+
+  describe :initialize do
+
+    it 'flattens the given classes' do
+      expect(subject.classes).to eql [PosableOne, PosableTwo]
+    end
+
+  end
+
+  describe :class_names do
+
+    it 'returns the names of the given classes' do
+      expect(subject.class_names).to eql ['PosableOne', 'PosableTwo']
+    end
+  end
+end
