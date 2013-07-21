81bb862335ca665bf6ef35cc24abb6d09e3bb311
Kevin Goslar <kevin.goslar@originate.com>
2013-07-22 06:25:04 +0800

    Skips coveralls coverage measuring on dev machine.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
-require 'coveralls'
-Coveralls.wear!
+if ENV['COVERALLS_CONFIG'] == 'nocoveralls'
+  require 'coveralls'
+  Coveralls.wear!
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
