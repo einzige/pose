2d25eac46e7af009bb130c4c205561c06377ff0f
Sergei Zinin <zinin@xakep.ru>
2013-08-17 21:16:34 +0800

    Words factory spec

--- spec/lib/pose/word_spec.rb
+++ spec/lib/pose/word_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 module Pose
   describe Word do
+    describe "::factory" do
+      let(:words) { ['1'] }
+
+      subject { described_class.factory(words) }
+
+      context "new word passed" do
+        it 'creates words' do
+          subject.should have(1).word
+          subject.first.text.should == '1'
+        end
+      end
+
+      context "existing word" do
+        before do
+          Word.create(text: '1')
+        end
+
+        it { expect { subject }.not_to change { Word.count } }
+        it { should == [Word.first] }
+      end
+    end
+
     describe 'class methods' do
       context 'with a SQL database' do
         before :each do
