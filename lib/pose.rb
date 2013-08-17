064593af80db954f7a9cc29b8a6ea5ad9e3614a5
Sergei Zinin <zinin@xakep.ru>
2013-08-17 16:50:35 +0800

    Extract jobs

--- lib/pose.rb
+++ lib/pose.rb
@@ -5,9 +5,13 @@ require 'pose/static_api'
 require 'pose/helpers'
 require 'pose/activerecord_base_additions'
 require 'pose/model_class_additions'
-require 'pose/railtie' if defined? Rails
 require 'pose/assignment'
 require 'pose/word'
+require 'pose/jobs/reindex_all'
+require 'pose/jobs/remove'
+require 'pose/jobs/vacuum'
+
+Dir["tasks/**/*.rake"].each { |ext| load ext } if defined?(Rake)
 
 module Pose
 end
